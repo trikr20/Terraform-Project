3dffe5838386ce89f38938535bbc0cb145d81959
trikr20 <119435705+trikr20@users.noreply.github.com>
2024-02-12 00:14:08 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_version = ">=1.5.6"
+}
+provider "aws" {
+  region = "us-east-1"
+
+} 
+
+resource "aws_s3_bucket" "example" {
+  bucket = "poc-121-141"
+  force_destroy = true
+#   logging {
+#     target_bucket = "ghh"
+#     target_prefix=""
+
+#   }
+  versioning {
+    enabled = true
+  }
+  tags = {
+    "owner" : "Tribhuwan"
+  }
+
+}
